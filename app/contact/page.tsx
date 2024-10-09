fd525a9ecc22fe4967a9d2cb4a6a4622e6322873
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-09 21:46:09 +0500

    assignments

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const Hello = () => {
   return (
     <div>
@@ -8,4 +7,4 @@ const Hello = () => {
   )
 }
 
-export default Hello
+export default Hello;
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 
 const Services = () => {
   return (
     <div style={{ color: 'white', backgroundColor: 'green', height: '100vh', padding: '20px' }}>
       <h1>About Page</h1>
-      <p>This is the About page with white text and a green background.</p>
+      <p style={{color:'pink'}}>This is the About page with white text and a green background.</p>
     </div>
   );
 };
